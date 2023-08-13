e49c776cd1552b8bd631d8c615da71418b78497c
Stefany2889 <37083884+Stefany2889@users.noreply.github.com>
2023-08-13 03:12:51 +0000

    Copilot second commit

--- member.js
+++ member.js
@@ -1,19 +1,8 @@
 function skillsMenber() {
-    return {
-        restrict: 'E',
-        templateUrl: 'views/skills-member.html',
-        controller: function ($scope) {
-            $scope.skills = [
-                { name: 'HTML5', value: 90 },
-                { name: 'CSS3', value: 80 },
-                { name: 'Javascript', value: 70 },
-                { name: 'AngularJS', value: 60 },
-                { name: 'NodeJS', value: 50 },
-                { name: 'MongoDB', value: 40 },
-                { name: 'PHP', value: 30 },
-                { name: 'MySQL', value: 20 },
-                { name: 'Python', value: 10 }
-            ];
-        }
-    };
-}
+  return {
+    restrict: 'E',
+    templateUrl: 'templates/skills-member.html',
+    controller: 'SkillsMemberController',
+    controllerAs: 'skillsMemberCtrl'
+  };
+}   
